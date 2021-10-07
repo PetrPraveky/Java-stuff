144991fee0058db0b25dfb359a24e3b1ec5f6ccb
PetrPraveky <56596287+PetrPraveky@users.noreply.github.com>
2021-10-07 20:49:26 +0200

    N-D Update 1

--- 1/N-Dimension/src/App.java
+++ 1/N-Dimension/src/App.java
@@ -1,6 +1,23 @@
+import java.lang.Math;
 // Program for computing stuff for higher dimensions
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Nrectangle rectangle = new Nrectangle(4);
+    }
+}
+class Nrectangle {
+    int dimension;
+    Nrectangle(int dimension) {
+        this.dimension = dimension;
+        System.out.println("Počet rohů: "+corners(dimension));
+        System.out.println("Počet hran: "+edges(dimension));
+    }
+    private int corners(int dimension) {
+        double a = 2.0; double b = dimension;
+        int ansCornerCount = (int) Math.pow(a, b); 
+        return ansCornerCount;
+    };
+    private int edges(int dimension) {
+        return dimension;
     }
 }
