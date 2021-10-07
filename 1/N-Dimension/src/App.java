dfb3cd9463544af8225f6b692dccfe90f9cad734
PetrPraveky <56596287+PetrPraveky@users.noreply.github.com>
2021-10-07 19:58:54 +0200

    N-Dimensions created

---
+++ 1/N-Dimension/src/App.java
@@ -0,0 +1,6 @@
+// Program for computing stuff for higher dimensions
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
