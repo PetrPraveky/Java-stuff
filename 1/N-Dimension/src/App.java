617f08a5d68f9c594c10914936d0390fc4433186
PetrPraveky <56596287+PetrPraveky@users.noreply.github.com>
2021-10-08 16:59:02 +0200

    N-D Update 3

--- 1/N-Dimension/src/App.java
+++ 1/N-Dimension/src/App.java
@@ -2,14 +2,20 @@ import java.lang.Math;
 // Program for computing stuff for higher dimensions
 public class App {
     public static void main(String[] args) throws Exception {
-        int dimNum = 10;
+        int dimNum = 8;
         new Nrectangle(dimNum);
+        // MathFuncs math = new MathFuncs();
+        // System.out.println(math.factorial(3));
     }
 }
 class Nrectangle {
     Nrectangle(int dimension) {
-        System.out.println("Počet rohů: "+corners(dimension));
-        System.out.println("Počet hran: "+edges(dimension));
+        System.out.println("Corner, edge, face and cell for "+dimension+"D hypercube");
+        System.out.println("");
+        System.out.println("Corner count: "+corners(dimension));
+        System.out.println("Edge count: "+edges(dimension));
+        System.out.println("Face count: "+faces(dimension));
+        System.out.println("Cell count: "+cells(dimension));
     }
     private int corners(int dimension) {
         double a = 2.0; double b = dimension;
@@ -20,5 +26,35 @@ class Nrectangle {
         double a = dimension; double b = (dimension-1);
         int ansEdgeCount = (int) (a*Math.pow(2.0, b));
         return ansEdgeCount;
+    }
+    private int faces(int dimension) {
+        if (dimension > 1) {
+            double a = 2.0; double b = dimension;
+            MathFuncs mfunc = new MathFuncs();
+            int ansFacesCount = (int) (Math.pow(2.0, (b-a))*((double)(mfunc.factorial((int)b)/(mfunc.factorial((int)a)*mfunc.factorial(((int)(b-a)))))));
+            return ansFacesCount;
+        } else {
+            return 0;
+        }
+    }
+    private int cells(int dimension) {
+        if (dimension > 2) {
+            double a = 3.0; double b = dimension;
+            MathFuncs mfunc = new MathFuncs();
+            int ansFacesCount = (int) (Math.pow(2.0, (b-a))*((double)(mfunc.factorial((int)b)/(mfunc.factorial((int)a)*mfunc.factorial(((int)(b-a)))))));
+            return ansFacesCount;
+        } else {
+            return 0;
+        }        
+    }
+}
+class MathFuncs {
+    public int factorial(int number) {
+        if (number == 0) {
+            return 1;
+        } else {
+            int ansFactorial = (number*factorial(number-1));
+            return ansFactorial;
+        }
     }
 }
