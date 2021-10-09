549881801401e7e4fbb6e9c203562c190a9f4235
PetrPraveky <56596287+PetrPraveky@users.noreply.github.com>
2021-10-09 22:04:41 +0200

    N-D Update 12

--- 1/N-Dimension/src/App.java
+++ 1/N-Dimension/src/App.java
@@ -1,8 +1,4 @@
 import funcs.AnsFolder;
-// import funcs.math.MathFuncs;
-// import funcs.Nrectangle;
-// import java.math.BigInteger;
-
 // Program for computing stuff for higher dimensions
 public class App {
     public static void main(String[] args) throws Exception {
