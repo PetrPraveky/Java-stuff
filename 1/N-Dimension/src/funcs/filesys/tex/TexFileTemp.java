dd5ea4f08b7d5661a7fdd0e0737a2c6aa6d4db36
PetrPraveky <56596287+PetrPraveky@users.noreply.github.com>
2021-10-20 19:38:55 +0200

    N-D Update 26

--- 1/N-Dimension/src/funcs/filesys/tex/TexFileTemp.java
+++ 1/N-Dimension/src/funcs/filesys/tex/TexFileTemp.java
@@ -28,16 +28,44 @@ public class TexFileTemp {
         System.out.println("\\centerline{\\Large{\\textbf{N-Dimensional Cube Terminology and Formulas}}}");
         texCommands.texMiddleLine(1);
         // System.out.println("\\vspace{\\baselineskip}");
-        // Terminology
         System.out.println("\\begin{flushleft}");
-        System.out.println("\\large{\\textbf{Base Terminology:}}\\newline");
-        System.out.println("\\large{haha}\\newline");
-        System.out.println("\\large{For Dimension $n$; Side lenght $a$}\\newline");
+        // Terminology
+        System.out.println("""
+            \\null\\large{\\quad Firstly let me talk about some terminilogy used in this document. There are some terms, that are not that obvious or maybe 
+            could mean something different than usual, so I will try my best to explain them.
+            }\\newline""");
+        System.out.println("""
+            \\null\\large{\\quad \"Vertex\" (or Vertices) is zero dimensional object (point), that is part of objects, in this scenario, cubes, in higher dimensions.
+            By connecting two vertices you make a line, or \"edge\". By crossing two same edges, thanks to second dimension (you can't do that in 1D), you make on vertex of square.
+            When you complete this square by the same technigue, it is named \"face\". Again, if you align three faces with same vertex and bend it
+            into third dimension, you create a one vertex of cube. Again by completing this cube, you can name in \"cell\". You can continue and name another objects in
+            higher dimensions, but I didn't do that and I only use these four terms. So Vertex mean zero dimensional part of object; Edge is one dimensional part of object of
+            certain lenght; Face is two dimensional part of object, square, made of four same edges; And Cell is third dimensional part of object, cube, made of six same
+            faces. You can find number of those in higher dimensional cubes later in this document. Also by \"leght a\" or \"side lenght a\" I mean specific lenght of edge.
+            }\\newline""");
+        System.out.println("""
+            \\null\\large{
+                \\quad In this part I'll explain some formulas used to compute data, that is in this document. There will be a bit more explaining and
+                lot of math. By \"$n$\" I mean the dimension number.
+            }\\newline
+            \\null\\large{
+                \\quad First formula used is equation for computing number of vertices \"$V$\" of $n$ dimensional object, cube. It is exponential function with
+                base of $2$ and exponent $n$. So the equation looks like this:} \\newline
+            \\null\\centerline\\Large{
+                $V = 2^{n}$
+            }\\newline""");
+
+        // System.out.println("\\large{\\textbf{Base Terminology:}}\\newline");
+        // System.out.println("\\large{haha}\\newline");
+        // System.out.println("\\large{For Dimension $n$; Side lenght $a$}\\newline");
         // System.out.println("\\vspace{\\baselineskip}");
         // Equations
-        System.out.println("\\textbf{Used Formulas:}\\newline");
-        System.out.println("\\large{- Equation for verticies count:}\\quad\\quad$2^{n}$\\newline");
-        System.out.println("\\large{- Equation for edge count:}\\quad\\quad$2^{n}$\\newline");
+        // System.out.println("\\textbf{Used Formulas:}\\newline");
+        // System.out.println("\\large{- Equation for verticies count:}\\quad\\quad$2^{n}$\\newline");
+        // System.out.println("\\large{- Equation for edge count:}\\quad\\quad$2^{n}$\\newline");
+
+
+
         System.out.println("\\end{flushleft}");
 
         System.out.println("\\newpage");
