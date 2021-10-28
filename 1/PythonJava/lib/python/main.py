c89916940cdd0089e8221c257bfc7ef4073a0933
PetrPraveky <56596287+PetrPraveky@users.noreply.github.com>
2021-10-28 22:47:17 +0200

    PJ Update 6

--- 1/PythonJava/lib/python/main.py
+++ 1/PythonJava/lib/python/main.py
@@ -17,53 +17,55 @@ workingdirectory=os.getcwd()
     
 # addition()
 def main():
-    # x = sympy.Symbol('x')
-    # print(sympy.solve(sympy.Eq(x, 3), x))
-    n = int(sys.argv[1])
-    coef = int(sys.argv[2])
-    V = Symbol('V')
-    R = 1
-    r = Symbol('r')
-    nCubeEquation = Eq(V, (pi**(n/2)/gamma((n/2)+1))*R**n)
-    print_latex(solve(nCubeEquation))
-    # print_latex(gammasimp(nCubeEquation))
-    # print_latex(nsimplify(nCubeEquation))
-    nCubeAns = solve(nCubeEquation)[0]
-    # print(nCubeAns)
-    # print("")
-    print_latex((nsimplify(nCubeAns/pi**coef))*(nsimplify(pi**coef))*(nsimplify(r**n)))
+    print(cos(pi))
+    print(sin(pi/9))
+    # # x = sympy.Symbol('x')
+    # # print(sympy.solve(sympy.Eq(x, 3), x))
+    # n = int(sys.argv[1])
+    # coef = int(sys.argv[2])
+    # V = Symbol('V')
+    # R = 1
+    # r = Symbol('r')
+    # nCubeEquation = Eq(V, (pi**(n/2)/gamma((n/2)+1))*R**n)
+    # print_latex(solve(nCubeEquation))
+    # # print_latex(gammasimp(nCubeEquation))
+    # # print_latex(nsimplify(nCubeEquation))
+    # nCubeAns = solve(nCubeEquation)[0]
+    # # print(nCubeAns)
+    # # print("")
+    # print_latex((nsimplify(nCubeAns/pi**coef))*(nsimplify(pi**coef))*(nsimplify(r**n)))
     
-    print("\n\n")
-    x1 = Symbol('x_1')
-    x2 = Symbol('x_2')
-    x3 = Symbol('x_3')
-    A = Symbol('A')
-    a1 = 2
-    a2 = 3
-    a3 = 4
-    # print_latex(x1*2)
-    print(solve(Eq(A, integrate(integrate(sqrt(1-a1/x1), (x1, 0, a1)))*x2, (x2, 0, a2)), A))
-    # print(
-    #     solve(
-    #         Eq(
-    #             A, (
-    #                 2**n*integrate(
-    #                     integrate(
-    #                         sqrt(
-    #                             (
-    #                                 1-(1-(a3**2/a1**2))*(x1**2/a1**2)-(1-(a3**2/a2**2))*(x2**2/a2**2)
-    #                             )/(
-    #                                 1-(x1**2/a1**2)-(x2**2/a2**2)
-    #                             )
-    #                         ), (x2, 0, (
-    #                             a2*sqrt(1-(x1**2/a1**2))
-    #                         ))
-    #                     ), (x1, 0, a1)
-    #                 )
-    #             )
-    #         ), A
-    #     )
-    # )
+    # print("\n\n")
+    # x1 = Symbol('x_1')
+    # x2 = Symbol('x_2')
+    # x3 = Symbol('x_3')
+    # A = Symbol('A')
+    # a1 = 2
+    # a2 = 3
+    # a3 = 4
+    # # print_latex(x1*2)
+    # print(solve(Eq(A, integrate(integrate(sqrt(1-a1/x1), (x1, 0, a1)))*x2, (x2, 0, a2)), A))
+    # # print(
+    # #     solve(
+    # #         Eq(
+    # #             A, (
+    # #                 2**n*integrate(
+    # #                     integrate(
+    # #                         sqrt(
+    # #                             (
+    # #                                 1-(1-(a3**2/a1**2))*(x1**2/a1**2)-(1-(a3**2/a2**2))*(x2**2/a2**2)
+    # #                             )/(
+    # #                                 1-(x1**2/a1**2)-(x2**2/a2**2)
+    # #                             )
+    # #                         ), (x2, 0, (
+    # #                             a2*sqrt(1-(x1**2/a1**2))
+    # #                         ))
+    # #                     ), (x1, 0, a1)
+    # #                 )
+    # #             )
+    # #         ), A
+    # #     )
+    # # )
     
 if __name__ == "__main__":
     main()
--- 1/PythonJava/src/App.java
+++ 1/PythonJava/src/App.java
@@ -1,23 +1,37 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
-public class App {
-    public static void main(String[] args) throws Exception {
-        int dimension = 3;
-        int coef = 1;
-        try {
-            ProcessBuilder builder = new ProcessBuilder(
-                "python",
-                System.getProperty("user.dir") + "\\lib\\python\\main.py", String.valueOf(dimension), String.valueOf(coef)
-            );
-            System.out.println(System.getProperty("user.dir") + "\\lib\\python\\main.py");
-            Process process = builder.start();
+interface test {
+    void set(String S);
+}
 
-            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-            // String lines=null;
-            System.out.println(reader.readLine());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+public class App {
+    void testdata(test fun, String S) {
+        fun.set(S);
+    }
+    void mytest (String S) {
+        System.out.println(S);
+    }
+    public static void main(String[] args) throws Exception {
+        App L = new App();
+        test func = L::mytest;
+        L.testdata(func, "How are you");
+
+        // int dimension = 3;
+        // int coef = 1;
+        // try {
+        //     ProcessBuilder builder = new ProcessBuilder(
+        //         "python",
+        //         System.getProperty("user.dir") + "\\lib\\python\\main.py", String.valueOf(dimension), String.valueOf(coef)
+        //     );
+        //     System.out.println(System.getProperty("user.dir") + "\\lib\\python\\main.py");
+        //     Process process = builder.start();
+
+        //     BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+        //     // String lines=null;
+        //     System.out.println(reader.readLine());
+        // } catch (Exception e) {
+        //     e.printStackTrace();
+        // }
     }
 }
